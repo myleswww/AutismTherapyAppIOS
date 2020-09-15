6cf73216b25a4164bcbe774a9473000e5d887d84
Nicole Selig <nicoleleeselig@gmail.com>
2020-09-15 16:46:57 -0400

    set up buttons..only 1 working

--- RiversideIOS/Controller/QuizGameViewController.swift
+++ RiversideIOS/Controller/QuizGameViewController.swift
@@ -11,6 +11,21 @@ import UIKit
 class QuizGameViewController: UIViewController {
 
     @IBOutlet weak var progressview: UIProgressView!
+    @IBOutlet weak var questionCounter: UILabel!
+    @IBOutlet weak var scoreCounter: UILabel!
+    @IBOutlet weak var questionView: UIImageView!
+    
+    //outlets for buttons
+ 
+    @IBOutlet weak var option1: UIButton!
+    
+    @IBOutlet weak var option2: UIButton!
+    
+    @IBOutlet weak var option3: UIButton!
+    
+    @IBOutlet weak var option4: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +36,29 @@ class QuizGameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    //all buttons are referenced to this function
+    //to reference any button, open up the story board, click on the assistant button, and control-click and drag the button into the code
+    @IBAction func answerPressed(_ sender: UIButton) {
+        
+        //control flow for buttons
+        switch sender {
+            case option1:
+                print("option1")
+                break
+            case option2:
+                print("option2")
+                break
+            case option3:
+                print("option3")
+                break
+            case option4:
+                print("option4")
+                break
+            default:
+                break
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
