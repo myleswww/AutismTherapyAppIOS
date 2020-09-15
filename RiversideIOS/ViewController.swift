ce675742ebc26d0263894c481bc871a1a5f5184c
Nicole Selig <nicoleleeselig@gmail.com>
2020-09-15 12:25:13 -0400

    Initial Commit

---
+++ RiversideIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiversideIOS
+//
+//  Created by Mom on 9/15/20.
+//  Copyright © 2020 Butler University EPICS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
