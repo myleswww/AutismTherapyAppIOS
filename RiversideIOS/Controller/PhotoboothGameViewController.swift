62a214ad5da72fda5c69bd773f703cd64be14d1f
Nicole Selig <nicoleleeselig@gmail.com>
2020-10-18 07:52:59 -0400

    figuring out cropping

--- RiversideIOS/Controller/PhotoboothGameViewController.swift
+++ RiversideIOS/Controller/PhotoboothGameViewController.swift
@@ -20,6 +20,13 @@ class PhotoboothGameViewController: UIViewController, UIImagePickerControllerDel
     
     let imagePicker = UIImagePickerController()
     
+    //finds the bounding boxes of faces in the image
+    lazy var faceDetectionRequest = VNDetectFaceRectanglesRequest(completionHandler: self.onFacesDetected)
+    let faceDetectionRequestHandler = VNSequenceRequestHandler()
+    
+    var rect: CGRect
+   
+    
     //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +47,14 @@ class PhotoboothGameViewController: UIViewController, UIImagePickerControllerDel
         // 'as?' is called optional binding
         //if the userPicked image can be cast as a UIImage datatype, then set in the imageView
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            imageView.image = userPickedImage
+            //imageView.image = userPickedImage
             
             //convert the image to greyscale
             let monoImage = userPickedImage.mono
+            
+            detectFaces(on: monoImage)
+            
+            
             
             //convert image to a core image image, which will allow us to use Vision and CoreML frameworks
             //'guard' statements are used as error handling..kind of like try and catches
@@ -51,24 +62,75 @@ class PhotoboothGameViewController: UIViewController, UIImagePickerControllerDel
                 fatalError("could not convert image to a CI Image")
             }
             
-            detect(image: ciImage)
+            let croppedCIImage = cropImage(image: ciImage, rect: rect)
+            
+            imageView.image = UIImage(ciImage: croppedCIImage)
+            
+            
+        
         }
         
         //dismiss the image picker and go back to the view controller
         imagePicker.dismiss(animated: true, completion: nil)
+        
        
     }
     
     
-    //MARK: detect
-    func detect(image: CIImage) {
+    func cropImage(image: CIImage, rect: CGRect) -> CIImage {
+        let croppedImage = image.cropped(to: rect)
+        return croppedImage
+    }
+    
+    //MARK: onFacesDetected
+    //completion handler. prints the result to the console upon face detection
+    func onFacesDetected(request: VNRequest, error: Error?) {
+        guard let faces = request.results as? [VNFaceObservation] else {
+            return
+        }
+        
+        for face in faces {
+            print("Found face at \(face.boundingBox)")
+            
+            let width = face.boundingBox.width
+            let height = face.boundingBox.height
+            let x = face.boundingBox.origin.x
+            let y = (1 - face.boundingBox.origin.y) - height
+            
+            rect = CGRect(x: x, y: y, width: width, height: height)
+        }
+    }
+    
+ 
+    
+    //MARK: detectFaces
+    func detectFaces(on image: UIImage){
+        //this handler performs different operations on a single image
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+        
+        //perform the request
+        DispatchQueue.global(qos: .userInitiated).async {
+            do{
+                try handler.perform([self.faceDetectionRequest])
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+    
+  
+    
+    
+    //MARK: classifyEmotion
+    func classifyEmotion(image: CIImage) {
         
         //create a new object of the emotion model
         //'try?' -- attempt to perform an operation. if it succeeds, it will be wrapped as an option, if not, then the result will be nil
         //the guard handles the nil and sends an error message.
         //VNCoreML Model is part of the Vision framework, and will allow us to analyze images.
         let config = MLModelConfiguration()
-            guard let model = try? VNCoreMLModel(for: CNNEmotions(configuration: config).model) else {
+            guard let model = try? VNCoreMLModel(for: EmotionClassificator(configuration: config).model) else {
             fatalError("Loading CoreML model failed.")
         }
         
